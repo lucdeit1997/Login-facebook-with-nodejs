0dc1ee5f032b839acd69bb2f0a3265d5698b2d93
vande1997 <lucdeit1997@gmail.com>
2020-06-21 19:22:36 +0700

    intit project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports={
+    "facebook_key"      :     "567464933928315", //Điền App ID của bạn vào đây
+    "facebook_secret"   :     "3c4847fddf37b22b274520f7e19342db", //Điền App Secret ở đây
+    "callback_url"      :     "http://localhost:3000/auth/facebook/callback",
+  }
